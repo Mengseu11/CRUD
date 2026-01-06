556992ee4b0d989d542b9f61d36809804605abee
Mengseu <mengseu049@gmail.com>
2026-01-06 14:04:27 +0700

    add

--- product-exercise.js
+++ product-exercise.js
@@ -1,43 +1,175 @@
 const tbody = document.getElementById('productTableBody')
+const id = document.getElementById('id')
 const name = document.getElementById('name')
 const price = document.getElementById('price')
 const color = document.getElementById('color')
 const category = document.getElementById('category')
-const model = document.getElementById('model')
 const form = document.getElementById('productForm')
+const btnCreate = document.getElementById('btnCreate')
+const btnUpdate = document.getElementById('btnUpdate')
+const filterSelect = document.getElementById('filterSelect')
+const searchInput = document.getElementById('searchInput')
 
 let products = []
-let autoId =1 
+let productIndex;
+document.addEventListener('DOMContentLoaded', function() {
+      console.log('first load')
+       loadFromCookie()
+      renderProducts()
+    //   const cookieName = getCookie('name')
+    //   if(cookieName === ""){
+    //     window.location.href = "login.html"
+    //   }
+    //   labelName.innerHTML = `<span style="color: red">${cookieName}</span>`
+});
+
+const FetchData = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')              
+        const data = await response.json()
+        console.log('data',data)
+        renderProducts(data);
+    } catch (error) {
+        console.log('error',error);
+        
+    }    
+}
+
+
 function createProduct(){
     const newProduct = {
-        id: autoId++,
+        id: products.length +1,
         name: name.value,
         price: parseFloat(price.value),
         color: color.value,
         category: category.value,
-        model: model.value
     }
 
     products.push(newProduct)
+    saveToCookie()
+    // saveToStorage()
     renderProducts()
     clearValue()
     console.log(newProduct);
     
 }
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function saveToCookie(){
+    document.cookie = `products = ${JSON.stringify(products)}`
+}
+function loadFromCookie(){
+    const data = getCookie('products')
+    if(data){
+        products = JSON.parse(data)
+    } 
+}
+// function saveToStorage(){
+//     localStorage.setItem("products",JSON.stringify(products))
+// }
+// function loadFromStorage(){
+//     const data = localStorage.getItem('products')
+//     if(data){
+//         products = JSON.parse(data)
+//     }
+// }
+// function removeFromStorage(){
+//     localStorage.removeItem('products')
+//     products=[]
+//     renderProducts()
+// }
+function editProduct(index){
+    const product = products[index]
+    // id.value = product.id
+    name.value = product.name
+    color.value = product.color
+    category.value = product.category
+    price.value = product.price
+    console.log(index);
+    console.log(product);
+    
+    
+    productIndex = index
+    console.log("hello");
+    openModal()
+    
+    btnCreate.style.display ='none'
+    btnUpdate.style.display = 'inline-block'
+
+}
+function updateProduct(){
+    const proUpdate = {
+        id: products.length +1,
+        name: name.value,
+        price: parseFloat(price.value),
+        color: color.value,
+        category: category.value
+    }
+    products[productIndex] = proUpdate
+    // saveToStorage()
+    closeModal()
+    btnCreate.style.display ='inline-block'
+    btnUpdate.style.display = 'none'
+    saveToCookie()
+    renderProducts()
+    
+
+}
+function deleteProduct(index){
+    products.splice(index,1)
+    console.log('delete',index);
+    
+   renderProducts() 
+}
+
+function searchFilter(){
+    const userSearch = searchInput.value.toLowerCase()
+
+    const result = products.filter(product => product.name.toLowerCase().includes(userSearch))
+    renderProducts(result)
+}
+function selectFilter(){
+    const productSelect = filterSelect.value
+        console.log('category', productSelect);
+        
+    let result = products
+    if(productSelect == 'Laptop'){
+        result = products.filter(product => product.category == 'Laptop')
+    }else if(productSelect == 'Phones'){
+        result = products.filter(product => product.category == 'Phones')
+    }else if(productSelect == 'Accessories'){
+        result = products.filter(product => product.category == 'Accessories')
+    }
+    renderProducts(result)
+
+}
  
-function renderProducts(){
+function renderProducts(productList = FetchData()){
     tbody.innerHTML = ""
-    products.forEach(product => {
+    productList.forEach((product, index) => {
         const tr = document.createElement('tr')
         tr.innerHTML = `
             <td class="px-6 py-4">${product.id}</td>
             <td class="px-6 py-4 font-medium text-gray-900">${product.name}</td>
-            <td class="px-6 py-4">${product.color}</td>
-            <td class="px-6 py-4">${product.category}</td>
-            <td class="px-6 py-4">${product.price}</td>
+            <td class="px-6 py-4">${product.email}</td>
+            <td class="px-6 py-4">${product.phone}</td>
+            <td class="px-6 py-4">${product.address.city}</td>
             <td class="px-6 py-4 text-center">
-              <button onclick="" class="bg-blue-600 px-2 py-2 rounded-md text-white hover:underline">Edit</button>
-              <button onclick="" class="bg-red-600 px-2 py-2 rounded-md text-white hover:underline">Delete</button>
+              <button onclick="editProduct(${index})" class="bg-blue-600 px-2 py-2 rounded-md text-white hover:underline">Edit</button>
+              <button onclick="deleteProduct(${index})" class="bg-red-600 px-2 py-2 rounded-md text-white hover:underline">Delete</button>
             </td>
         `
         tbody.append(tr)
@@ -48,7 +180,13 @@ function clearValue(){
     price.value = ""
     color.value= ""
     category.value = ""
-    model.value = ""
     
 }
 
+
+function openModal(){
+    document.getElementById('crud-modal').classList.remove('hidden')
+}
+function closeModal(){
+    document.getElementById('crud-modal').classList.add('hidden')
+}
